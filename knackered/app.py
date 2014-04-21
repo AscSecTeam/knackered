cbbd48c071d7670c42f85fd9907580e7a4a91eb8
brettlangdon <brett@blangdon.com>
2014-04-20 21:57:14 -0400

    app section will always be there

--- knackered/app.py
+++ knackered/app.py
@@ -8,10 +8,9 @@ def create_app(settings):
     app = Flask(__name__)
 
     # load "app" namespace as flask settings
-    app_settings = getattr(settings, "app", Section("app", {}))
     app.config.update(
         (key.upper(), value)
-        for key, value in app_settings.data.iteritems()
+        for key, value in settings.app.data.iteritems()
     )
     # store all settings under app.config to be able
     # to access settings from anywhere
